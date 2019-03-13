d1ec56e177b7ac4444e98f4cf1a71697969762b3
mipcsoft <mipcsoft@gmail.com>
2019-03-12 19:45:17 -0500

    proyecto symfony

---
+++ _gps/notas.py
@@ -0,0 +1,1 @@
+href="{{ asset('css/font.css'}}"
---
+++ src/Controller/WebController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebController extends AbstractController
+{
+    /**
+     * @Route("/", name="web")
+     */
+    public function index()
+    {
+        return $this->render('web/index.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+}
