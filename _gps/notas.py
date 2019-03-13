6b89ded2f7e520eeaff32f1aa4805ea32afe4657
mipcsoft <mipcsoft@gmail.com>
2019-03-13 13:48:22 -0500

    organizacion inicio plantilla unify

--- _gps/notas.py
+++ _gps/notas.py
@@ -1,1 +1,15 @@
 href="{{ asset('css/font.css'}}"
+
+
+git
+mipcsoft
+<Mbcd1969>
+
+
+composer create-project symfony/website-skeleton gps-s
+mysqladmin --user=root password "aguila2019"
+
+
+bd
+
+bd_informes_poli
