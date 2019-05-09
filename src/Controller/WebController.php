86ca28f37045c593e3b6eb29a57d0a10168c5ae6
mipcsoft <mipcsoft@gmail.com>
2019-05-08 20:40:18 -0500

    templates para pestañas contactenos y politicas

--- src/Controller/WebController.php
+++ src/Controller/WebController.php
@@ -16,4 +16,69 @@ class WebController extends AbstractController
             'controller_name' => 'WebController',
         ]);
     }
+
+    /**
+     * @Route("/contact/maps", name="contact_maps")
+     */
+    public function contact_maps()
+    {
+        return $this->render('web/contact/contact_maps.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/contact/messenger", name="contact_messenger")
+     */
+    public function contact_messenger()
+    {
+        return $this->render('web/contact/contact_messenger.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+    /**
+     * @Route("/politics/corporate", name="politics_corporate")
+     */
+    public function politics_corporate()
+    {
+        return $this->render('web/politics/politics_corporate.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+    /**
+     * @Route("/politics/resource", name="politics_resource")
+     */
+    public function politics_resource()
+    {
+        return $this->render('web/politics/politics_resource.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+    /**
+     * @Route("/politics/quality", name="politics_quality")
+     */
+    public function politics_quality()
+    {
+        return $this->render('web/politics/politics_quality.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+    /**
+     * @Route("/politics/confidentiality", name="politics_confidentiality")
+     */
+    public function politics_confidentiality()
+    {
+        return $this->render('web/politics/politics_confidentiality.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+
 }
+
