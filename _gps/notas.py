98ba6ce3095d0c040b8cbc550366c9e010ee0d47
alejofix <alejo_fix@hotmail.com>
2019-06-08 12:06:08 -0500

    inicio base template

--- _gps/notas.py
+++ _gps/notas.py
@@ -18,3 +18,8 @@ https://medium.com/@marcelochoque/primeros-pasos-en-symfony-4-a9a94a270171
 composer require symfony/apache-pack		y
 composer require symfony/orm-pack
 composer require symfony/maker-bundle --dev
+
+
+https://blog.wearedrew.co/3-pasos-para-integrar-whatsapp-con-tu-sitio-web
+
+<img src="{{ asset('assets/img/manrique/iconfinder_flag_colombia_92836.png') }}
